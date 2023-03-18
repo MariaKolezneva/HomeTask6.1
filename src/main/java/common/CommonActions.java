7cfb15c0b338d657e0d6dafcaf7319c1cd17ef4d
MariaKolezneva <77171892+MariaKolezneva@users.noreply.github.com>
2023-03-19 01:40:06 +0400

    Update CommonActions.java

--- src/main/java/common/CommonActions.java
+++ src/main/java/common/CommonActions.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class CommonActions {
     public static final String chromeDriverPath = "C:/chromeDriver/chromedriver.exe";
     private static ChromeOptions chromeOptions = new ChromeOptions();
-    public static final String website = "https://onliner.by/";
-    public static final String websiteCatalog = "https://catalog.onliner.by/";
     public static WebDriver createDriver(){
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
         WebDriver driver = new ChromeDriver(chromeOptions);
